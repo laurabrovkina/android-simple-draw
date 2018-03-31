e7bcde14a19b26f6d14b9c7d04e1849ef0e5e086
laurabrovkina <laurabrovkina@mail.ru>
2018-03-31 17:59:50 +1300

    The simple line drawer

---
+++ app/src/main/java/com/example/admin/opaic/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.admin.opaic;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CanvasView canvasView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        canvasView = (CanvasView) findViewById(R.id.canvas);
+    }
+    public void clearCanvas(View v){
+        canvasView.clearCanvas();
+    }
+}
